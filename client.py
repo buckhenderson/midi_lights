0c3436666eb6e9e902ef2881eb7c4fee094fa9da
ben <buckhendersonunionbuster23@gmail.com>
2020-03-22 22:48:28 -0700

    adjust code to accept pedal

--- client.py
+++ client.py
@@ -5,27 +5,26 @@ import pickle
 import mido
 
 
-HOST='192.168.1.36'
-PORT=2031
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = '192.168.1.36'
+PORT = 2031
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 this_list = []
-velocities = []
 with mido.open_input('MIDI Matrix Encoder:MIDI Matrix Encoder MIDI 1 20:0') as inport:
     while True:
         for msg in inport:
-            midi_in = (msg.type, msg.note - 21, msg.velocity)
-            velocities.append(midi_in[2])
-            set_velocities = set(velocities)
-            velocities = [x for x in set_velocities]
-            print(sorted(velocities))
-            # print(midi_in)
-            message = pickle.dumps(midi_in)
-            s.send(message)
-            if msg.type == 'note_on':
-                this_list.append(msg)
-            if msg.type == 'note_off':
-                this_list = [x for x in this_list if x.note != msg.note]
+            if hasattr(msg, 'note'):
+                midi_in = (msg.type, msg.note - 21, msg.velocity)
+                message = pickle.dumps(midi_in)
+                s.send(message)
+                if msg.type == 'note_on':
+                    this_list.append(msg)
+                if msg.type == 'note_off':
+                    this_list = [x for x in this_list if x.note != msg.note]
+            if hasattr(msg, 'control'):
+                midi_in = ('pedal', False if msg.value == 0 else True)
+                message = pickle.dumps(midi_in)
+                s.send(message)
             print(this_list)
 
 
@@ -37,5 +36,3 @@ with mido.open_input('MIDI Matrix Encoder:MIDI Matrix Encoder MIDI 1 20:0') as i
 #         time.sleep(1)
 # except:
 #     s.close()
-
-
