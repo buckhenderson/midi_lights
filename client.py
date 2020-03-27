e9645d19bf0b677054313f3dcb7fbd04a9825a34
ben <buckhendersonunionbuster23@gmail.com>
2020-03-27 15:01:18 -0700

    reverse light order

--- client.py
+++ client.py
@@ -18,7 +18,7 @@ with mido.open_input('MIDI Matrix Encoder:MIDI Matrix Encoder MIDI 1 20:0') as i
                     this_type = 'on'
                 else:
                     this_type = 'off'
-                this_note = msg.note - 21
+                this_note = 87 - (msg.note - 21)
                 this_velocity = msg.velocity
                 this_time = time.time()
                 this_message = '{0:<3},{1:<2},{2:<3},{3:6f}'.format(this_type, this_note, this_velocity, this_time)
