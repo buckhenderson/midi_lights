d4d8b0ef7cc9d3930ef5c3853b83554d1054146a
ben <buckhendersonunionbuster23@gmail.com>
2020-04-06 22:53:35 -0700

    fix master

--- master.py
+++ master.py
@@ -8,7 +8,10 @@ while True:
     print('1: manual')
     print('2: player')
     user_input = input()
+    print('{}, {}'.format(user_input, type(user_input)))
     if user_input == '1':
+        print('selecting client')
         client.client()
     if user_input == '2':
+        print('selecting player')
         player.player_piano()
